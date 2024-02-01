9ceaba48ccad64623dd44f2796dbfb5b69196606
Qweran <105790249+Qweran@users.noreply.github.com>
2024-02-01 14:10:03 +0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -20,6 +20,7 @@ fun main()
     println("Введите сумму прошлых переводов (если нет, то пж на ноль тыкнете, мне проверки писать лень)")
 
     val summaPred = timeWrite.nextDouble()
+
    // println( timeManager(timerWr))
     println(zadanie2(nameBanka,summa,summaPred))
 
@@ -53,54 +54,60 @@ fun timeManager(timeWrite: Int) : String
 }
 fun zadanie2(nameBanka:String, summa: Double, summaPred:Double)
 {
-    var summ = summa
-    if(nameBanka.equals(""))
+  var summ = summa
+    if(summa > 150001){ println("Слишком большая сумма на текущий день")   }
+    if(summaPred> 600001){ println("Слишком большая сумма на текущий месяц") }
+    if (nameBanka.equals(""))
     {
         nameBanka === "VKPlay"
     }
 
-    if(nameBanka.equals("VKPay") )
+    if (nameBanka.equals("VKPay"))
     {
-        println("Без комисии сегодня" + " суммарная оплата с вас $summa" )
+        if(summ>15000)
+        { println("Слишком большая сумма на текущий день")
 
-
-    }
-    if(nameBanka.equals("Visa")||nameBanka.equals("Мир"))
-    {
-        summ *= 0.75
-        summ += summa
-        println("Комисии сегодня" + " суммарная оплата с вас $summ" )
-
-    }
-    else
-    {
-        if(summaPred+summa >300 && summa+summaPred < 75000)
+        }
+        if(summaPred> 40000)
         {
-            if(summa > 300)
-            {
+            println("Слишком большая сумма на текущий месяц")
+        }
 
-                println("Без комисии сегодня" + " суммарная оплата с вас $summa" )
-
-            }
-            else
-            {
-                summ *=0.06
-                summ += summa
-                println("Комисии сегодня" + " суммарная оплата с вас $summ" )
+        println("Без комисии сегодня" + " суммарная оплата с вас $summa")
 
 
+    }
+    if (nameBanka.equals("Visa") || nameBanka.equals("Мир")) {
+        if(summa > 35) {
+            summ *= 0.75
+            summ += summa
+            println(" суммарная оплата с вас $summ")
+        }
+        else{
+            println("Тут меньше необходимого лимита в 35")
 
-            }
+        }
+    }
+    if(nameBanka.equals("Mastercard") || nameBanka.equals("Maestro"))
+    {
+        if (summaPred + summa > 300 && summa + summaPred < 75000)
+        {
+            println("Без комисии сегодня" + " суммарная оплата с вас $summa")
+
         }
         else
         {
-            summ *= 0.06
+            summ *= 0.6
             summ += summa
-            println("Комисии сегодня" + " суммарная оплата с вас $summ" )
+            println("Комисии сегодня" + " суммарная оплата с вас $summ")
 
         }
+
+
+
+    }
 
     }
 
 
-}
+
